80d5471444bfa6aefa146816eada77e2a86923e0
Adam Egnell <71374893+Eggetxd@users.noreply.github.com>
2022-11-26 19:31:39 +0100

    Update Temp.js

--- Temp.js
+++ Temp.js
@@ -12,3 +12,62 @@ while i < length(A)
     i ← i + 1
 end while
 return A
+
+
+
+# SELECTION SORT #
+
+i ← 0
+while i < length(A)-1
+    minIndex ← i
+    j ← i + 1
+    while j < length(A)
+        if A[j] < A[minIndex] then
+            minIndex ← j
+        end if
+    end while
+    if minIndex != i then
+        swap A[i] and A[minIndex]
+    end if
+    i ← i + 1
+end while
+return A
+
+
+
+# MERGE SORT (top-down) #
+
+mergesort(A as list)
+    if length(A) == 1 then return A
+
+    left ← A[0]..A[length(A)/2]
+    right ← A[length(A)/2]..A[length(A)-1]
+
+    left ← mergesort(left)
+    right ← mergesort(right)
+
+    return merge(left, right)
+end func
+
+merge(A as list, B as list):
+    C ← []
+
+    while length(A) > 0 and length(B) > 0
+        if A[0] > B[0] then
+            add B[0] to the end of C
+            remove B[0] from B
+        else
+            add A[0] to the end of C
+            remove A[0] from A
+        end if
+    end while
+    while length(A) > 0
+        add A[0] to the end of C
+        remove A[0] from A
+    end while
+    while length(B) > 0
+        add B[0] to the end of C
+        remove B[0] from B
+    end while
+    return C
+end func
