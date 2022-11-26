805c74085c3faa56f932948d156553b2f0085066
Adam <adam.egnell@hotmail.com>
2022-11-26 17:35:17 +0100

    pog

--- main.js
+++ main.js
@@ -5,25 +5,33 @@ let amount = 10
 
 updateAmount()
 function updateAmount() {
-    console.log(document.querySelector("[data-amount]").value);
     if (!(document.querySelector("[data-amount]").value == "")) {
         amount = document.querySelector("[data-amount]").value;
-        console.log("pog");
     }
 
-    console.log(amount);
+    while (graph.hasChildNodes()) {
+        graph.removeChild(graph.firstChild);
+      }
 
     for (let i = 0; i < amount; i++) {
         let pillar = document.createElement("div");
         pillar.setAttribute("value", i);
         pillar.classList.add("pillar");
-        pillar.style.height = i*20 + "px";
+        pillar.style.height = (i+1)*5 + "px";
         graph.append(pillar);
     }
 }
 
 function shuffle() {
-
+    console.log("shuffle");
+    for (let i = 0; i < amount; i++) {
+        let temp = graph. children[i];
+        let rand = Math.floor(Math.random() * amount);
+        console.log(temp.value + ", " + rand);
+        graph.children[i].replaceChild(newNode, graph.children[rand]);
+        graph.children[rand].replaceChild(newNode, temp);
+    }
+    
 }
 
 function sort() {
